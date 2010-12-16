8b37ede60359f0dee6199a1dd4ded722f7a832b2
rmaneschi <rmaneschi@71f76536-a0e9-3b76-2bbb-c3cb5497e6af>
2010-12-16 17:23:21 +0000

    Supression des classes pour le N3 translator (pb déporté dans la config)

--- trunk/Raja/src/Server/Translator/N3TranslatorHorizontal.java
+++
@@ -1,7 +0,0 @@
-package Server.Translator;
-
-public class N3TranslatorHorizontal extends N3Translator {
-  public RDF getMetaInfo() {
-  }
-
-}
--- trunk/Raja/src/Server/Translator/N3TranslatorVertical.java
+++
@@ -1,7 +0,0 @@
-package Server.Translator;
-
-public class N3TranslatorVertical extends N3Translator {
-  public RDF getMetaInfo() {
-  }
-
-}
