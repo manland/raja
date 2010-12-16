e0084c59d7b87e9d61891d32c8b9d55a34e6f312
rmaneschi <rmaneschi@71f76536-a0e9-3b76-2bbb-c3cb5497e6af>
2010-12-16 18:02:24 +0000

    Mise à jour UML + ajout des prefix dans le translator

--- trunk/Raja/src/Server/Translator/N3/N3Translator.java
+++ trunk/Raja/src/Server/Translator/N3/N3Translator.java
@@ -1,4 +1,6 @@
 package Server.Translator.N3;
+import java.util.Vector;
+
 import Query.SelectQuery;
 import com.hp.hpl.jena.query.ResultSet;
 
@@ -6,18 +8,19 @@ public abstract class N3Translator implements IN3Translator {
 	
 	private String n3File;
 	private String getMetaInfo;
+	private Vector<String> prefix;
 	
-	public N3Translator(String n3File, String getMetaInfo)
+	public N3Translator(String n3File, String getMetaInfo, Vector<String> prefix)
 	{
 		this.n3File = n3File;
 		this.getMetaInfo = getMetaInfo;
+		this.prefix = prefix;
 	}
 	
 	public ResultSet select(SelectQuery query) 
 	{
 		String requete = query.getQuery();
 		return null;
-		
 	}
 
 	@Override
