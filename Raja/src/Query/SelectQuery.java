80f872b9620b9ca51fbad8d36ef10dc7664209bb
novak.audrey <novak.audrey@71f76536-a0e9-3b76-2bbb-c3cb5497e6af>
2010-12-21 16:59:47 +0000

    réglage de l'expression régulière pour qu'elle prenne en compte les UNION

--- Raja/src/Query/SelectQuery.java
+++ Raja/src/Query/SelectQuery.java
@@ -16,60 +16,11 @@ public class SelectQuery extends Query
   private Vector<String> selection = new Vector<String>();
   private Vector<String> where = new Vector<String>();
   
-  /**
-   * Parse the SELECT given query.
-   */
-  public void parseQuery(String query) throws MalformedQueryException 
-  {
-	  where.removeAllElements();
-	  selection.removeAllElements();
-	  String req = "^SELECT ([\\?[a-zA-Z_]+ ]+) WHERE \\{+([[[\\??[a-zA-Z_]+ {0,1}|[a-zA-Z_]+:[a-zA-Z_]+ {0,1}]{3}] \\. ]+)\\}+";
-	  Pattern pattern = Pattern.compile(req);
-	  Matcher m = pattern.matcher(query);
-//	      [\\{+[[\\??[a-zA-Z_]+:[a-zA-Z_]+ ]{3}\\.?]+\\}+ [UNION]? ]+
-	  
-//	^SELECT ([\\?[a-zA-Z_]+ ]+) WHERE \\{+([((\\??[a-zA-Z_]+ {0,1}|[a-zA-Z_]+:[a-zA-Z_]+ {0,1}){3}) \\. ])+\\}+
-	  if (query.matches(req))
-	  {
-		  while(m.find())
-		  {
-			  String req_where = m.group(2);
-			  String [] tab_where = req_where.split("\\.");
-			  for(int i=0; i<tab_where.length;i++)
-			  {
-				  String [] tab_temp = tab_where[i].split(" ");
-				  for(int j=0; j<tab_temp.length;j++)
-				  {
-					  if(tab_temp[j].matches("[a-zA-Z_]+:[a-zA-Z_]+ {0,1}"))
-					  {
-						  where.add(tab_temp[j]);
-					  }
-				  }
-			  }
-			  System.out.println("taille where : "+tab_where.length);
-			  String req_select = m.group(1);
-			  String [] tab_select = req_select.split(" ");
-			  for(int i=0; i<tab_select.length;i++)
-			  {
-				  selection.add(tab_select[i]);
-			  }
-		  }
-	  }
-	  else
-	  {
-		  throw new MalformedQueryException(query, "Requete mal formee");
-	  }
-  }
+  public static final String DROITE = "droite";
+  public static final String GAUCHE = "gauche";
+  public static final String MILIEU = "milieu";
+   
 
-  /**
-   * Rebuild and return query.
-   */
-  public String getQuery() 
-  {
-	  return query;
-  }
-
-<<<<<<< .mine
 	/**
 	 * Parse the SELECT given query.
 	 */
@@ -192,22 +143,4 @@ public class SelectQuery extends Query
 		System.out.println("requte : "+str_prefix+q.getQuery());
 		return str_prefix+q.getQuery();
 	}
-
-=======
-  public void setQuery(String query)
-  {
-	  this.query = query;
-  }
-  
-  public Vector<String> getSelection()
-  {
-	  return selection;
-  }
-  
-  public Vector<String> getWhere()
-  {
-	  return where;
-  }
-  
->>>>>>> .r125
 }
